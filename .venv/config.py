99cba15397be032ad98886d2433732eaae684b8b
wholocx <57823316+wholocx@users.noreply.github.com>
2024-07-05 17:35:13 +0300

    Update and rename config.py to config.gitignore

--- .venv/config.py
+++
@@ -1,6 +0,0 @@
-from pydantic import SecretStr
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-WEATHER_TOKEN = "dca57a54f83e04b1ff0abbe39e64f8df"
-BOT_TOKEN = "6219000255:AAH83kGJuT2HtYj53nWQ5gkMLLiZVG9lMwc"
